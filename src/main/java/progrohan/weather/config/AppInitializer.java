648ef448253967da614c3dc72e947746b07fc1c3
Andrey <aprishlov@gmail.com>
2025-02-01 21:43:19 +0300

    feat: created configs for spring and rest

---
+++ src/main/java/progrohan/weather/config/AppInitializer.java
@@ -0,0 +1,23 @@
+package progrohan.weather.config;
+
+import jakarta.validation.constraints.NotNull;
+import org.springframework.lang.Nullable;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{WebConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/*"};
+    }
+}
---
+++ src/main/java/progrohan/weather/config/WebConfig.java
@@ -0,0 +1,25 @@
+package progrohan.weather.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@ComponentScan("progrohan.weather")
+@EnableTransactionManagement
+@EnableWebMvc
+@Import({DataSourceConfig.class, HibernateConfig.class})
+public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public MappingJackson2HttpMessageConverter jsonConverter() {
+        return new MappingJackson2HttpMessageConverter(new ObjectMapper());
+    }
+
+}

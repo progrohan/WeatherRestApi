335058964f41d0e459b2cebc5dbb08600e582723
Andrey <aprishlov@gmail.com>
2025-02-03 16:43:24 +0300

    feat: created findByNameMethod

--- src/main/java/com/progrohan/weather/repository/UserRepository.java
+++ src/main/java/com/progrohan/weather/repository/UserRepository.java
@@ -1,9 +1,14 @@
 package com.progrohan.weather.repository;
 
+import com.progrohan.weather.exception.DataBaseException;
 import com.progrohan.weather.model.entity.User;
+import jakarta.persistence.NoResultException;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public class UserRepository extends BaseRepository<Long, User>{
@@ -13,5 +18,18 @@ public class UserRepository extends BaseRepository<Long, User>{
         super(User.class, sessionFactory);
     }
 
+    public Optional<User> findByName(String name){
+        try(Session session = sessionFactory.openSession()){
+            User user = session
+                    .createQuery("FROM User WHERE login = :login", User.class)
+                    .setParameter("login", name)
+                    .getSingleResult();
+            return Optional.of(user);
+        }catch (NoResultException e){
+            return Optional.empty();
+        }catch (Exception e){
+            throw new DataBaseException("Problem with finding user by name!");
+        }
+    }
 
 }

e1c4b6c0ee7b43fbed446b74942a899a3dae1b04
Andrey <aprishlov@gmail.com>
2025-02-03 16:44:09 +0300

    feat: created createSession method

--- src/main/java/com/progrohan/weather/service/AuthService.java
+++ src/main/java/com/progrohan/weather/service/AuthService.java
@@ -1,30 +1,72 @@
 package com.progrohan.weather.service;
 
+import com.progrohan.weather.dto.SessionDTO;
+import com.progrohan.weather.dto.UserLoginDTO;
 import com.progrohan.weather.dto.UserRegRequestDTO;
 import com.progrohan.weather.dto.UserResponseDTO;
 import com.progrohan.weather.exception.DataExistException;
+import com.progrohan.weather.exception.DataNotFoundException;
+import com.progrohan.weather.exception.InvalidDataException;
+import com.progrohan.weather.mapper.SessionMapper;
+import com.progrohan.weather.mapper.UserMapper;
+import com.progrohan.weather.model.entity.Session;
 import com.progrohan.weather.model.entity.User;
+import com.progrohan.weather.repository.SessionRepository;
 import com.progrohan.weather.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class AuthService {
 
     private final UserRepository userRepository;
+    private final SessionRepository sessionRepository;
+    private final SessionMapper sessionMapper;
+    private final UserMapper userMapper;
 
-    public UserResponseDTO create(UserRegRequestDTO userRequestDTO){
+    public UserResponseDTO createUser(UserRegRequestDTO userRequestDTO){
         try{
-            User user = new User(null, userRequestDTO.getLogin(), userRequestDTO.getPassword());
+            User user = userMapper.toEntity(userRequestDTO);
 
             user = userRepository.save(user);
 
-            return new UserResponseDTO(user.getLogin());
+            return userMapper.toDto(user);
         }catch(DataExistException e){
             throw new DataExistException("User with name" + userRequestDTO.getLogin() + " already exists");
         }
 
+    }
+
+    public SessionDTO createSession(UserResponseDTO userResponseDTO){
+        LocalDateTime time = LocalDateTime.now();
+
+        SessionDTO sessionDTO = SessionDTO.builder()
+                .uuid(UUID.randomUUID())
+                .user(userResponseDTO)
+                .expiresAt(time.plusDays(7))
+                .build();
+
+        Session session = sessionRepository.save(sessionMapper.toEntity(sessionDTO));
+
+        return sessionMapper.toDto(session);
+    }
+
+    public UserResponseDTO login(UserLoginDTO userLoginDTO){
+
+        Optional<User> userOptional = userRepository.findByName(userLoginDTO.getLogin());
+
+        User user = userOptional.orElseThrow(() -> new DataNotFoundException("User not found!"));
+
+        if(!Objects.equals(user.getPassword(), userLoginDTO.getPassword()))
+            throw new InvalidDataException("Password is incorrect!");
+
+        return userMapper.toDto(user);
     }
 
 }

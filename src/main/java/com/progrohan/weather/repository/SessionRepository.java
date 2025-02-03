d4b2dc17a08393d60eadef0dcac3f7fe12e84d78
Andrey <aprishlov@gmail.com>
2025-02-03 16:42:45 +0300

    feat: created SessionRepository

---
+++ src/main/java/com/progrohan/weather/repository/SessionRepository.java
@@ -0,0 +1,18 @@
+package com.progrohan.weather.repository;
+
+import com.progrohan.weather.model.entity.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public class SessionRepository extends BaseRepository<UUID, Session>{
+
+    @Autowired
+    public SessionRepository(SessionFactory sessionFactory) {
+        super(Session.class, sessionFactory);
+    }
+
+}

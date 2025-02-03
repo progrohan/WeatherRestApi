00d3edb38dd80ab2cba02c85853e21217fec0c6a
Andrey <aprishlov@gmail.com>
2025-02-03 16:44:27 +0300

    feat: created login method

--- src/main/java/com/progrohan/weather/controller/AuthController.java
+++ src/main/java/com/progrohan/weather/controller/AuthController.java
@@ -1,18 +1,23 @@
 package com.progrohan.weather.controller;
 
+import com.progrohan.weather.dto.SessionDTO;
 import com.progrohan.weather.dto.UserLoginDTO;
 import com.progrohan.weather.dto.UserRegRequestDTO;
 import com.progrohan.weather.dto.UserResponseDTO;
 import com.progrohan.weather.service.AuthService;
 import com.progrohan.weather.util.DataValidator;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
@@ -26,19 +31,30 @@ public class AuthController {
 
         dataValidator.checkUserReg(userRequestDTO);
 
-        UserResponseDTO userResponseDTO = authService.create(userRequestDTO);
+        UserResponseDTO userResponseDTO = authService.createUser(userRequestDTO);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(userResponseDTO);
     }
 
     @PostMapping("/login")
-    public String logIn(@RequestBody UserLoginDTO userLoginDTO){
+    public ResponseEntity<String> logIn(@RequestBody UserLoginDTO userLoginDTO){
 
         dataValidator.checkUserLogin(userLoginDTO);
 
+        UserResponseDTO userResponseDTO = authService.login(userLoginDTO);
 
+        SessionDTO session = authService.createSession(userResponseDTO);
 
-        return "Login successful";
+        ResponseCookie cookie = ResponseCookie.from("sessionId", session.getUuid().toString())
+                .httpOnly(true)
+                .path("/")
+                .maxAge(3600)
+                .build();
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .header(HttpHeaders.SET_COOKIE, cookie.toString())
+                .body("Login successful");
     }
 
     @PostMapping("/logout")

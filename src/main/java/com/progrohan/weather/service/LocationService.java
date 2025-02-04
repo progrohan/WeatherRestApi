31a1855223954616c50e5709d3175498d55808d9
Andrey <aprishlov@gmail.com>
2025-02-04 22:48:49 +0300

    feat: created LocationService and findLocationsByName method

---
+++ src/main/java/com/progrohan/weather/service/LocationService.java
@@ -0,0 +1,48 @@
+package com.progrohan.weather.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.progrohan.weather.dto.LocationDTO;
+import com.progrohan.weather.exception.ApiException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+
+
+
+@Service
+@RequiredArgsConstructor
+public class LocationService {
+
+    private final Environment env;
+    private final RestTemplate restTemplate;
+    private final ObjectMapper objectMapper;
+
+
+    public List<LocationDTO> findLocationsByName(String name){
+        try{
+            String url = String
+                    .format("http://api.openweathermap.org/geo/1.0/direct?q=%s&limit=5&appid=%s",
+                            name, env.getRequiredProperty("api.key")) ;
+
+            String response = restTemplate.getForObject(url,  String.class);
+
+            List<LocationDTO> locations = null;
+
+            locations = objectMapper.readValue(response, new TypeReference<>() {});
+
+            if (locations.isEmpty()) throw new ApiException("Nothing is found");
+
+            return locations;
+        } catch (JsonProcessingException e) {
+            String s = "s";
+            throw new ApiException("Problem with finding locations");
+        }
+    }
+
+}

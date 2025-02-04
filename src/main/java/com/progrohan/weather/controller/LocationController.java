2ca219dcca24672beecdc1889cb765178ad68bb6
Andrey <aprishlov@gmail.com>
2025-02-04 22:49:13 +0300

    feat: created searchLocation method

--- src/main/java/com/progrohan/weather/controller/LocationController.java
+++ src/main/java/com/progrohan/weather/controller/LocationController.java
@@ -1,18 +1,27 @@
 package com.progrohan.weather.controller;
 
+import com.progrohan.weather.dto.LocationDTO;
+import com.progrohan.weather.service.LocationService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/locations")
+@RequiredArgsConstructor
 public class LocationController {
 
+    private final LocationService locationService;
+
     @GetMapping("/search")
-    public String searchLocation(){
-        return "location";
+    public List<LocationDTO> searchLocation(@RequestParam String name){
+        return locationService.findLocationsByName(name);
     }
 
     @PostMapping

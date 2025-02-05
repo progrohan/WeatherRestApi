c6468bd0856daaabefc43d120acc1eda80241c42
Andrey <aprishlov@gmail.com>
2025-02-05 15:48:32 +0300

    feat: created addLocation method

--- src/main/java/com/progrohan/weather/controller/LocationController.java
+++ src/main/java/com/progrohan/weather/controller/LocationController.java
@@ -1,13 +1,19 @@
 package com.progrohan.weather.controller;
 
 import com.progrohan.weather.dto.LocationDTO;
+import com.progrohan.weather.dto.SessionDTO;
+import com.progrohan.weather.dto.UsersLocationDTO;
+import com.progrohan.weather.mapper.LocationMapper;
+import com.progrohan.weather.service.AuthService;
 import com.progrohan.weather.service.LocationService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +26,8 @@ import java.util.List;
 public class LocationController {
 
     private final LocationService locationService;
+    private final AuthService authService;
+    private final LocationMapper locationMapper;
 
     @GetMapping("/search")
     public ResponseEntity<List<LocationDTO>>  searchLocation(@RequestParam String name){
@@ -30,8 +38,21 @@ public class LocationController {
     }
 
     @PostMapping
-    public String addLocation(){
-        return "Added location";
+    public ResponseEntity<LocationDTO> addLocation(
+            @RequestBody LocationDTO locationDTO,
+            HttpServletRequest request){
+
+        SessionDTO session = authService.getSessionFromCookies(request.getCookies());
+
+        UsersLocationDTO usersLocationDTO = locationMapper.toUsersDTO(locationDTO);
+
+        usersLocationDTO.setUser(session.getUser());
+
+        LocationDTO location = locationService.saveLocation(usersLocationDTO);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(location);
     }
 
     @GetMapping

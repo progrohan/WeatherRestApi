3ee487e4f2224bef421db6a88002fa54485b7102
Andrey <aprishlov@gmail.com>
2025-02-05 15:44:06 +0300

    chore: added uuid mapping

--- src/main/java/com/progrohan/weather/mapper/SessionMapper.java
+++ src/main/java/com/progrohan/weather/mapper/SessionMapper.java
@@ -7,19 +7,20 @@ import org.mapstruct.Mapping;
 import org.mapstruct.Mappings;
 import org.mapstruct.factory.Mappers;
 
-@Mapper(componentModel = "spring", uses = UserMapper.class)
+@Mapper(componentModel = "spring",
+        uses = {UserMapper.class, UUIDMapper.class})
 public interface SessionMapper {
 
     SessionMapper INSTANCE = Mappers.getMapper(SessionMapper.class);
 
     @Mappings({
-            @Mapping(source = "uuid", target = "id"),
+            @Mapping(source = "uuid", target = "id", qualifiedByName = "uuidToString"),
             @Mapping(source = "user", target = "userId"),
     })
     Session toEntity(SessionDTO dto);
 
     @Mappings({
-            @Mapping(source = "id", target = "uuid"),
+            @Mapping(source = "id", target = "uuid", qualifiedByName = "stringToUuid"),
             @Mapping(source = "userId", target = "user"),
     })
     SessionDTO toDto(Session entity);

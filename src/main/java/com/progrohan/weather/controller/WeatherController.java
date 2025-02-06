8209b25d6475dee6046a5d7d2b5af3a167d46904
Andrey <aprishlov@gmail.com>
2025-02-06 21:51:48 +0300

    feat: created getWeather method

--- src/main/java/com/progrohan/weather/controller/WeatherController.java
+++ src/main/java/com/progrohan/weather/controller/WeatherController.java
@@ -1,16 +1,31 @@
 package com.progrohan.weather.controller;
 
+import com.progrohan.weather.dto.LocationDTO;
+import com.progrohan.weather.dto.weather.WeatherResponceDTO;
+import com.progrohan.weather.service.WeatherService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/weather")
 public class WeatherController {
 
+    private final WeatherService weatherService;
+
     @GetMapping
-    public String getWeather(){
-        return "weather";
+    public ResponseEntity<WeatherResponceDTO> getWeather(@RequestBody LocationDTO locationDTO){
+
+        WeatherResponceDTO weather = weatherService.getByLocation(locationDTO);
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(weather);
     }
 
 }

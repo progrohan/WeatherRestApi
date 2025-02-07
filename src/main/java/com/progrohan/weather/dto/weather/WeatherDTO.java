60af179499678bc339559209f55f58b0b8a3222e
Andrey <aprishlov@gmail.com>
2025-02-07 23:38:11 +0300

    feat: moved weather classes to WeatherDTO

--- src/main/java/com/progrohan/weather/dto/weather/WeatherDTO.java
+++ src/main/java/com/progrohan/weather/dto/weather/WeatherDTO.java
@@ -1,11 +1,13 @@
 package com.progrohan.weather.dto.weather;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @Data
@@ -23,4 +25,53 @@ public class WeatherDTO {
 
     private Clouds clouds;
 
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Weather {
+
+        private String main;
+
+        private String description;
+
+    }
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Main {
+
+        private BigDecimal temp;
+
+        @JsonProperty("feels_like")
+        private BigDecimal feelsLike;
+
+    }
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Clouds {
+
+        @JsonProperty("all")
+        private BigDecimal cloudiness;
+
+    }
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public class Wind {
+
+        private BigDecimal speed;
+
+        private BigDecimal deg;
+
+    }
+
 }
+
--- src/main/java/com/progrohan/weather/dto/weather/WeatherResponceDTO.java
+++ src/main/java/com/progrohan/weather/dto/weather/WeatherResponceDTO.java
@@ -1,6 +1,6 @@
 package com.progrohan.weather.dto.weather;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;

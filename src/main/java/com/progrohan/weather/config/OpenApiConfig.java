a558ff91875028c9481609bbf1a57ee9cab18be5
Andrey <aprishlov@gmail.com>
2025-02-16 21:22:02 +0300

    feat: created OpenApiConfig

---
+++ src/main/java/com/progrohan/weather/config/OpenApiConfig.java
@@ -0,0 +1,21 @@
+package com.progrohan.weather.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("My API")
+                        .version("1.0")
+                        .description("API documentation"));
+    }
+
+}
